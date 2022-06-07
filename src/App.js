dc66d4894273560d5433fd84d7fff4f93be74d6c
aiman.wahizam <aiman.wahizam@nology.io>
2022-06-07 15:34:16 +0100

    added employee data and starting to inc props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.scss';
-import Tickettracker from './components/Tickettracker/Tickettracker';
+import "./App.scss";
+import Tickettracker from "./components/Tickettracker/Tickettracker";
+import employees from "./data/employees.js";
 
 function App() {
   return (
-    <div className='main'>
-          <h1>Ticket Tracker</h1>
-          <div className='main__trackers'>
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-            <Tickettracker />
-          </div>
+    <div className="main">
+      <h1>Ticket Tracker</h1>
+      <div className="main__trackers">
+        <Tickettracker employeeArray={employees} />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+        <Tickettracker />
+      </div>
     </div>
-
   );
 }
 
--- src/components/Tickettracker/Tickettracker.jsx
+++ src/components/Tickettracker/Tickettracker.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Tickettracker.scss";
 
-const Tickettracker = () => {
+const Tickettracker = (props) => {
+  const { employeeArray } = props;
+
   return (
     <div className="card">
-        <h2 className="card__text">Name</h2>
-        <h2 className="card__text">Role</h2>
+      <h2 className="card__text">Name</h2>
+      <h2 className="card__text">Role</h2>
       <div className="card__counter">
         <h3>Counter</h3>
         <h3>0</h3>
         <div className="card__buttons">
-            <button>-</button>
-            <button>+</button>
+          <button>-</button>
+          <button>+</button>
         </div>
       </div>
     </div>
