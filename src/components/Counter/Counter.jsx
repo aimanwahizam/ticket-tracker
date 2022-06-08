bc2aeb74419e084b6b2e95875442af28fdd82ec4
aiman.wahizam <aiman.wahizam@nology.io>
2022-06-08 12:48:33 +0100

    made counter changeable by buttons

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Counter.scss";
-import Button from '../Button/Button';
+import Button from "../Button/Button";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="counter">
-    <h3>Counter</h3>
-    <h3>0</h3>
-    <div className="counter__buttons">
-        <Button buttonText="-"/>
-        <Button buttonText="+"/>
+      <h3>Counter</h3>
+      <h3>{counter}</h3>
+      <div className="counter__buttons">
+        <div onClick={decreaseCounter}>
+          <Button buttonText="-" />
+        </div>
+        <div onClick={increaseCounter}>
+          <Button buttonText="+" />
+        </div>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
